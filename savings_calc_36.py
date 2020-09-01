e84759d1cafee124cd84bfea29fc8e22fb7dd9fe
Shawn Lentz <shawn.lentz@gmail.com>
2020-08-31 16:01:56 -1000

    Did some cleaning to get rid of notes

--- savings_calc_36.py
+++ savings_calc_36.py
@@ -1,6 +1,5 @@
 # Removed total_cost from time_to_save function
 
-# I feel like I could make an object here and use these 5 values as attributes
 total_cost = 500000
 down_payment = int(total_cost * 0.25)
 current_savings = 0
@@ -63,39 +62,6 @@ def ideal_savings():
 
 ideal_savings()
 
-#down_payment =  int(total_cost * 0.25) # This is the ideal savings rate to get within 100 of down_payment
-'''portion_down_payment = range((down_payment - 100), (down_payment + 101)) # This is the down_payment range
-start = 0
-end = 10000
-#mid = midpoint(start, end)
-counter = 0
-savings = 0
-while current_savings not in portion_down_payment: # While savings * mid not in target range
-	mid = round(midpoint(start, end), 2)
-	rate = round(mid / 10000, 4)
-	savings = time_to_save(annual_salary, current_savings, rate)[0]
-	months = time_to_save(annual_salary, current_savings, rate)[1]
-	counter += 1
-	#print(f"This is savings: {savings}")
-	if int(savings) in portion_down_payment:
-		print(f"{months} months")
-		break
-	elif savings > max(portion_down_payment):
-		end = mid
-		#print(f"{rate} Greater than")
-	elif savings < min(portion_down_payment):
-		start = mid
-		#print(f"{rate} Less than")
-
-print(savings)
-print(f"Search steps: {counter}")
-print(f"Savings rate: {rate}")'''
-
-
-
-
-
-
 
 # PSET 1/B function
 portion_saved = .4411
@@ -111,7 +77,6 @@ def time_to_save(total_cost, portion_down_payment, annual_salary, current_saving
 		current_savings += monthly_saved
 		current_savings = round(current_savings + investment, 2)
 		months += 1
-	#print(f"It will take {months} months to save up enough for a down payment.")
 	return months
 
 
